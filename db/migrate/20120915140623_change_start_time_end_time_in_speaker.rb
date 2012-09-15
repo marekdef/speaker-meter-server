25bce1496155c61bf6da6d14c0b07bf6c625fc99
Marek Defeciński <marekdef@gmail.com>
2012-09-15 16:20:31 +0200

    Changed migration of time adding colums

--- db/migrate/20120915140623_change_start_time_end_time_in_speaker.rb
+++ db/migrate/20120915140623_change_start_time_end_time_in_speaker.rb
@@ -6,8 +6,8 @@ def change
   add_column :speakers, :end_time, :date
 
   Speaker.reset_column_information
-  Speaker.find_each { |s| s.update_attribute(:start_time, c.start_time_bak) } 
-  Speaker.find_each { |s| s.update_attribute(:end_time, c.end_time_bak) } 
+  Speaker.find_each { |s| s.update_attribute(:start_time, s.start_time_bak) } 
+  Speaker.find_each { |s| s.update_attribute(:end_time, s.end_time_bak) } 
   remove_column :speakers, :start_time_bak
   remove_column :speakers, :end_time_bak
 end
