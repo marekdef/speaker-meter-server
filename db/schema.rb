e69fd837292721dbee4c0fe45dcc98a9602b3230
Wojciech Jachowicz <wojciech.jachowicz@mobica.com>
2013-09-27 13:44:46 +0200

    Add language, and venue_id to speakers.json

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130925052528) do
+ActiveRecord::Schema.define(:version => 20130925054608) do
 
   create_table "ratings", :force => true do |t|
     t.integer  "rating"
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(:version => 20130925052528) do
     t.text     "name"
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
-    t.text     "presentation",   :limit => 255,  :default => "", :null => false
+    t.text     "presentation",                   :default => "", :null => false
     t.string   "description",    :limit => 1024
     t.boolean  "visible"
-    t.text     "bio",            :limit => 255
+    t.text     "bio"
     t.integer  "time_slot_id"
     t.string   "language"
     t.integer  "average_rating"
